db3f7465cd7146320f7b797bc8a4a7e61766fad5
LuMoR <rmllmr@ukr.net>
2017-03-07 00:05:22 +0300

    RCache RCache.getRCache RCache.puRCache RCache.checkRCache

--- src/main/java/com/mysite/RCach.java
+++ src/main/java/com/mysite/RCach.java
@@ -8,31 +8,40 @@ import java.util.Map;
  * Created by LuMoR on 06.03.2017.
  */
 class RCach {
-    private Map<String, Integer> cachValue = new HashMap<String, Integer>();
-    private Map<String, Long> cachTimeLive = new HashMap<String, Long>();
+    private Map<String, Integer> cacheValue = new HashMap<String, Integer>();
+    private Map<String, Long> cacheTimeLive = new HashMap<String, Long>();
 
-    public Integer getRCach(String keyRCach){
+    public Integer getRCache(String keyRCache){
+        checkRCache();
         long timeLive;
         long timeNow = new Date().getTime();
-        if (cachTimeLive.containsKey(keyRCach)){
-            timeLive = cachTimeLive.get(keyRCach);
-            if (timeLive < timeNow){
-              return cachValue.get(keyRCach);
-            }else{
-              return -1;
-            }
-        }else{
-            return -1;
+        if (cacheTimeLive.containsKey(keyRCache)){
+            return cacheValue.get(keyRCache);
         }
+        return null;
     };
 
 
-    public void putRCach(String keyRCach, Integer valueRCash, Integer timeLive){
-
-
-
+    public void putRCache(String keyRCache, Integer valueRCashe, int timeLive){
+        checkRCache();
+        if (timeLive > 0) {
+            long timeLiveToCache = timeLive * 1000 + System.currentTimeMillis();
+            cacheValue.put(keyRCache, valueRCashe);
+            cacheTimeLive.put(keyRCache, timeLiveToCache);
+        }
     }
 
+    private void checkRCache(){
+        long timeNow = System.currentTimeMillis();
+        for (String rElementCache: cacheTimeLive.keySet()
+                ) {
+            if (cacheTimeLive.get(rElementCache) < timeNow){
+                cacheTimeLive.remove(rElementCache);
+                cacheValue.remove(rElementCache);
+            }
+        }
+
+    }
     public RCach(){
 
     }
